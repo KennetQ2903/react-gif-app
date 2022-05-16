fdd0ec3a06051a22ceb96785cc0e5f7fa68cba76
KennetQ2903 <kennetquiroz@gmail.com>
2022-05-16 10:20:14 -0600

    gif app v2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import './App.css'
 import { ListOfGifs } from './components/ListOfGifs'
 import { Link, Route } from 'wouter'
+import 'primereact/resources/themes/vela-blue/theme.css'
+import 'primereact/resources/primereact.min.css' // core css
+import 'primeicons/primeicons.css'
 
 function App () {
   return (
@@ -9,8 +12,8 @@ function App () {
       <section className='App-content'>
         <h1>App</h1>
         <Link to='/gif/Nami'>Gifs de Nami</Link>
-        <Link to='/gif/Nico+Robin'>Gifs de Nico Robin</Link>
-        <Link to='/gif/Boa+Hancock'>Gifs de Boa Hancock</Link>
+        <Link to='/gif/Nico Robin'>Gifs de Nico Robin</Link>
+        <Link to='/gif/Boa Hancock'>Gifs de Boa Hancock</Link>
         <Route
           component={ListOfGifs}
           path='/gif/:keyword'
